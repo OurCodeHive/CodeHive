f93788159de05d6f178df296d22df524a211e23e
KimMinSik <rlaalstlr0108@gmail.com>
2023-07-19 17:33:32 +0900

    [REFACTOR/back] 0719 KMS UserController isolate

--- BE/src/main/java/com/spoon/sok/domain/user/controller/UserController.java
+++ BE/src/main/java/com/spoon/sok/domain/user/controller/UserController.java
@@ -1,4 +1,19 @@
 package com.spoon.sok.domain.user.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/api")
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/login/user")
+    public ResponseEntity<?> login(@RequestBody UserRequestDto requestDto) {
+        return userService.login(requestDto);
+    }
 }
