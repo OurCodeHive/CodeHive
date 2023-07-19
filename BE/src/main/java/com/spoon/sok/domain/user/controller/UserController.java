7d7c740b4236990659a53aae7ca1685050b34eb4
KimMinSik <rlaalstlr0108@gmail.com>
2023-07-19 21:05:40 +0900

    [FEAT/back] 0719 KMS Create UserRequestDto

--- BE/src/main/java/com/spoon/sok/domain/user/controller/UserController.java
+++ BE/src/main/java/com/spoon/sok/domain/user/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.spoon.sok.domain.user.controller;
 
+import com.spoon.sok.domain.user.dto.UserRequestDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,8 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @RequestMapping("/api")
+@RequiredArgsConstructor
 public class UserController {
-
     private final UserService userService;
 
     @PostMapping("/login/user")
