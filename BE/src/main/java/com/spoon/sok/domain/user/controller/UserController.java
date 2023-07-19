0a4e165d5b235902fd47fb93af605aff2d43cbc1
KimMinSik <rlaalstlr0108@gmail.com>
2023-07-20 01:22:55 +0900

    [FEAT/back] 0720 KMS Create UserController

--- BE/src/main/java/com/spoon/sok/domain/user/controller/UserController.java
+++ BE/src/main/java/com/spoon/sok/domain/user/controller/UserController.java
@@ -2,20 +2,64 @@ package com.spoon.sok.domain.user.controller;
 
 import com.spoon.sok.domain.user.dto.UserRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class UserController {
+
     private final UserService userService;
 
     @PostMapping("/login/user")
     public ResponseEntity<?> login(@RequestBody UserRequestDto requestDto) {
         return userService.login(requestDto);
+    }
+
+    @PostMapping("/signup")
+    public boolean signup(@RequestBody UserSignupRequestDto requestDto) {
+        return userService.signup(requestDto);
+    }
+
+    @GetMapping("/check/{nickname}")
+    public boolean checkNickname(@PathVariable String nickname) {
+        return userService.checkNickname(nickname);
+    }
+
+    @PostMapping("/find/password")
+    public ResponseEntity<?> changePassword(@RequestBody UserRequestDto requestDto) {
+        return userService.changePassword(requestDto);
+    }
+
+    @GetMapping("/email/auth")
+    public ResponseEntity<?> sendEmail(@RequestParam("email") String email) {
+        return userService.sendEmail();
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout() {
+        return userService.logout();
+    }
+
+    @PutMapping("/info/{nickname}")
+    public ResponseEntity<?> changeUserInfo(@PathVariable String nickname) {
+        return userService.changeUserInfo();
+    }
+
+    @PostMapping("resign")
+    public ResponseEntity<?> deleteAccount() {
+        return userService.deleteAccount();
+    }
+
+    @PostMapping("resign/chat")
+    public ResponseEntity<?> deleteChat() {
+        return userService.deleteChat();
+    }
+
+    @PostMapping("resign/file")
+    public ResponseEntity<?> deleteFiles() {
+        return userService.deleteFiles();
     }
 }
