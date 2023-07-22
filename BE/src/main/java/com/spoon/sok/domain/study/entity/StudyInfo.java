13f59a2902880e75772f30c1469d47cce2ec4da2
gleehave <gleehave@gmail.com>
2023-07-22 18:57:08 +0900

    [FEAT/back] GH 230722 StudyInfo Entity

---
+++ BE/src/main/java/com/spoon/sok/domain/study/entity/StudyInfo.java
@@ -0,0 +1,42 @@
+package com.spoon.sok.domain.study.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Getter
+@Entity
+@Table(name = "STUDY_INFO")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class StudyInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "studyinfo_id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "studyroom_size")
+    private String studyroomSize;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "profileimage")
+    private String profileImage;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt; // 스터디 그룹 생성일
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date endAt; // 스터디 그룹 종료일
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "users_id", nullable = false)
+//    private User users; // 스터디 그룹의 장
+}
