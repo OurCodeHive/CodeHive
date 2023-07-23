44d7a9c8bb0562b7e90642fb088cb0811f0ddd23
gleehave <gleehave@gmail.com>
2023-07-23 13:52:29 +0900

    [FEAT/back] GH 230723 Native Query (USER, STUDYINFO, STUDYAPPOINTMENT)

--- BE/src/main/java/com/spoon/sok/domain/study/repository/StudyRepository.java
+++ BE/src/main/java/com/spoon/sok/domain/study/repository/StudyRepository.java
@@ -3,11 +3,17 @@ package com.spoon.sok.domain.study.repository;
 import com.spoon.sok.domain.study.entity.StudyAppointment;
 import com.spoon.sok.domain.study.entity.StudyInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface StudyRepository extends JpaRepository<StudyInfo, Long> {
 
-    List<StudyAppointment> findByNicknameStudyMeetings(String nickname);
+    @Query(value = "SELECT sa.* " +
+                   "FROM USER u " +
+                   "JOIN STUDYINFO si ON u.user_id = si.user_id " +
+                   "JOIN STUDYAPPOINTMENT sa ON si.studyinfo_id = sa.studyinfo_id " +
+                   "WHERE u.user_id = :=nickname", nativeQuery = true)
+    List<StudyAppointment> findByNicknameStudyMeetings(@Param("nickname") int nickname);
 }
--- BE/src/main/java/com/spoon/sok/domain/study/service/StudyService.java
+++ BE/src/main/java/com/spoon/sok/domain/study/service/StudyService.java
@@ -3,11 +3,13 @@ package com.spoon.sok.domain.study.service;
 import com.spoon.sok.domain.study.entity.StudyAppointment;
 import com.spoon.sok.domain.study.repository.StudyRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -16,7 +18,9 @@ public class StudyService {
     private final StudyRepository studyRepository;
 
     public List<StudyAppointment> getStudyMeeting(String nickname) {
-        return studyRepository.findByNicknameStudyMeetings(nickname);
+        log.info("StudyService입니다. {} ", nickname);
+//        int userId = userRepository.findById(nickname);
+        return studyRepository.findByNicknameStudyMeetings(1);
     }
 
 }
