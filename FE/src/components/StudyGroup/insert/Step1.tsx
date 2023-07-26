8e5e1a279585b5ac830d58bdf9ef6bc1b24374c8
JM <k3371548@gmail.com>
2023-07-27 00:01:39 +0900

    [FEAT/front] 0727 JM CustomEditor complete, StudyInsertPopup style add

--- FE/src/components/StudyGroup/insert/Step1.tsx
+++ FE/src/components/StudyGroup/insert/Step1.tsx
@@ -1,9 +1,12 @@
+import { useRef } from 'react';
 import CustomEditor from "@/components/Util/CustomEditor";
 
 const StudyInsert1Step: React.FC = () => {
+    const descInput:React.RefObject<HTMLInputElement> = useRef<HTMLInputElement>(null);
+
     return (
-        <div className="col-12">
-            <form className="col-12 form_style_0_con" encType="multipart/form-data">
+        <form className="col-12" encType="multipart/form-data">
+            <div className="col-12 mb37 form_style_0_con">
                 <div className="col-12 form_style_0">
                     <div className="col-12 col-md-0 label_box">
                         <label htmlFor="studyInsertTitle" className="essential">제목</label>
@@ -14,10 +17,11 @@ const StudyInsert1Step: React.FC = () => {
                 </div>
                 <div className="col-12 form_style_0">
                     <div className="col-12 col-md-0 label_box">
-                        <label htmlFor=""></label>
+                        <span>내용</span>
                     </div>
                     <div className="col-12 col-md-0 input_box">
-                        <CustomEditor/>
+                        <input type="hidden" ref={descInput}/>
+                        <CustomEditor resultInput={descInput} />
                     </div>
                 </div>
                 <div className="col-12 form_style_0 type_date_range">
@@ -32,11 +36,21 @@ const StudyInsert1Step: React.FC = () => {
                 </div>
                 <div className="col-12 form_style_0 type_file">
                     <div className="col-12 col-md-0 label_box">
-                        <label htmlFor="">대표 이미지</label>
+                        <label htmlFor="studyInsertProFileImg">대표 이미지</label>
+                    </div>
+                    <div className="col-12 col-md-0 input_box">
+                        <input type="file" id="studyInsertProFileImg" name="studyInsertProFileImg" className="input_style_0" accept=".png, .jpg, .jpeg"/>
+                        <div className="col-12 mb12 addr_text">형식은 png, jpg, jpeg만 가능합니다.</div>
+                        <label htmlFor="studyInsertProFileImg" className="col-0 btn_style_0 bg_black border_none">이미지 등록</label>
+                        <ul className="col-12 file_info_con"></ul>
                     </div>
                 </div>
-            </form>
-        </div>
+            </div>
+            <div className="col-12 tc btn_style_0_con">
+                <button className="btn_style_0 mr15">취소</button>
+                <button className="btn_style_0 bg_point0">만들기</button>
+            </div>
+        </form>
     )
 };
 
