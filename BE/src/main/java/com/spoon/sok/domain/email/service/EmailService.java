8be959168f8651ad202e273697d587d2bd69654b
KimMinSik <rlaalstlr0108@gmail.com>
2023-07-26 16:41:31 +0900

    [FIX/back] 0726 KMS Modify Email Auth Logic

--- BE/src/main/java/com/spoon/sok/domain/email/service/EmailService.java
+++ BE/src/main/java/com/spoon/sok/domain/email/service/EmailService.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 
@@ -64,6 +65,7 @@ public class EmailService {
                     .email(email)
                     .authCode(code)
                     .limitTime(LocalDateTime.now().plusMinutes(3))
+                    .isauth(0)
                     .build();
 
             emailRepository.save(emailAuth);
@@ -107,6 +109,7 @@ public class EmailService {
                     .email(email)
                     .authCode(code)
                     .limitTime(LocalDateTime.now().plusMinutes(3))
+                    .isauth(0)
                     .build();
 
             emailRepository.save(emailAuth);
@@ -123,6 +126,7 @@ public class EmailService {
         return new ResponseEntity<Map<String, Object>>(result, status);
     }
 
+    @Transactional
     public ResponseEntity<?> verify(EmailAuthVerifyDto requestDto) {
         Map<String, Object> result = new HashMap<>();
         HttpStatus status;
@@ -147,6 +151,7 @@ public class EmailService {
 
         if (verifyEmail.get().getEmail().equals(requestDto.getEmail())
                 && verifyEmail.get().getAuthCode().equals(requestDto.getAuthCode())) {
+
             verifyEmail.get().updateIsAuth(1);
 
             result.put("status", 200);
