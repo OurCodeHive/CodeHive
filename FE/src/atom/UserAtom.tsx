6e759060eb45c6082c0224ee5f0ebd32fefa215b
Daen12 <sdy971213@gmail.com>
2023-07-26 12:17:03 +0900

    [FEAT/front] 0726 DY update useratom

--- FE/src/atom/UserAtom.tsx
+++ FE/src/atom/UserAtom.tsx
@@ -1,13 +1,19 @@
 import { atom } from "recoil";
+function getAccessToken(){
+    //유효하지 않으면
+    //api로 보내기 
+    // return accessToken
+}
 
 export const userState = atom({
   key: "countState", // 전역적으로 고유한 값
   default: {
+    userIdx : "",
     nickname : "",
     email : "",
-    password : "",
-    emailAuthCode : "",
+    password : "",//
     accessToken : "",
     refreshToken:""
   } // 초깃값
 });
+
