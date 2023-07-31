13c816e188f3df504191763c69bde295517c874f
Daen12 <sdy971213@gmail.com>
2023-07-31 17:57:57 +0900

    [FEAT/front] 0731 interceptor file created

--- FE/src/api/http.ts
+++ FE/src/api/http.ts
@@ -5,18 +5,63 @@
 */
 
 import axios from "axios";
-import {refresh, refreshError} from "./token";
-// axios.defaults.withCredentials = true;
+// import {refresh, refreshError} from "./token";
 
 //인증이 필요한 axios instance
-const http =  axios.create({
-    baseURL : 'http://localhost:8080/api',
-    // baseURL : 'https://hiveapi.minsungblog.com/api',
+const authHttp =  axios.create({
+    baseURL : import.meta.env.VITE_APP_SERVER,
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
     },
 });
+
+authHttp.interceptors.request.use(
+    (config) => {
+      const accessToken = localStorage.getItem("accessToken")
+      config.headers.common["Authorization"] = `Bearer ${accessToken}`
+      return config
+    }
+)
+
+  restApi.interceptors.response.use(
+    async (response) => {
+      const { config } = response;
+      const originalRequest = config;
+  
+      if (response?.data?.code == 40300) {
+        const refresh_token = await getStorage("refreshToken")
+        return await restApi.get(`/account/token?refreshToken=${refresh_token}`)
+          .then(async (res) => {
+            if (res.data.message === "OK") {
+              setStorage("token", res.data.payload.access_token)
+              originalRequest.headers.Authorization = `${res.data.payload.access_token}`;
+              return axios(originalRequest);
+            } 
+  
+          }).catch((err) => {
+            console.log(err)
+          })
+      }
+  
+      return response;
+    },
+    async (error) => {
+  
+      console.log(error, '^^***')
+      throw error
+  
+    }
+  )
+
+
+
+
+
+
+
+
+
 // http.interceptors.request.use(refresh, refreshError);
 
 //인증이 불필요한 axios instance
