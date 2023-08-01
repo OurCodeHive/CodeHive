f245bc83b84b4bc0ce5514b7a3a5c0b41b431ad1
지민성 <jiminsung@naver.com>
2023-08-01 21:44:38 +0900

    [FEAT/back] 0801 MS update DTO and userId

--- BE/src/main/java/com/spoon/sok/domain/chat/dto/ChatWebSocketDto.java
+++ BE/src/main/java/com/spoon/sok/domain/chat/dto/ChatWebSocketDto.java
@@ -1,14 +1,11 @@
 package com.spoon.sok.domain.chat.dto;
 
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
-@Data
-@ToString
-@Setter @Getter
-public class ChatDto {
+@Getter
+@Setter
+public class ChatWebSocketDto {
 
     private Long userId;
     private Long studyRoomId;
--- BE/src/main/java/com/spoon/sok/domain/chat/dto/LambdaRequestDto.java
+++ BE/src/main/java/com/spoon/sok/domain/chat/dto/LambdaRequestDto.java
@@ -1,16 +1,15 @@
 package com.spoon.sok.domain.chat.dto;
 
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-@Data
-@ToString
 @Setter
 @Getter
+@ToString
 public class LambdaRequestDto {
 
+    private Long userId;
     private String name;
     private String code;
     private String input;
