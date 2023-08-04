05a666ad453ec42f76c7f96f71297489717ad566
정민 <k3371548@naver.com>
2023-08-04 10:08:51 +0900

    [FIX/front] 0804 JM bug fix and merge

--- FE/src/components/StudyGroup/notice/list/List.tsx
+++ FE/src/components/StudyGroup/notice/list/List.tsx
@@ -1,10 +1,12 @@
 import {useEffect, useState, useRef} from 'react';
-import NoticeFilter from './item/Filter';
-import NoticeListItem from './item/ListItem';
-import TableListStyle from '@/utils/List/Table/css/ListTable.module.css';
 import { StudyNoticeType } from '@/type/StudyNoticeType';
 import { getNoticeList } from '@/api/study';
+import NoticeFilter from './item/Filter';
+import NoticeListItem from './item/ListItem';
 import Pagination, { PaginationType } from '@/utils/Pagination/Pagination';
+import {ContentsPopup} from "@/utils/Popup";
+import TableListStyle from '@/utils/List/Table/css/ListTable.module.css';
+
 
 const studyinfoId = Number(new URLSearchParams(location.search).get("studyinfoId"));
 
@@ -44,7 +46,7 @@ const NoticeList = () => {
     return (
         <div className="col-12 pt50 pr20 pb20 pl20">
             <NoticeFilter/>
-            <div className={`col-12 ${TableListStyle.table_style_0_con}`}>
+            <div className={`col-12 mb30 ${TableListStyle.table_style_0_con}`}>
                 <table className={`col-12 ${TableListStyle.table_style_0}`}>
                     <colgroup>
                         {WidGroup.map((item, index) => <col key={index} width={item}/>)}
@@ -61,6 +63,7 @@ const NoticeList = () => {
             </div>
             <div className="col-12">
                 <Pagination PaginationInfo={PaginationInfo} />
+                <button className={`plus_btn`}>공지사항 추가 버튼</button>
             </div>
         </div>
     )
--- FE/src/utils/Pagination/Pagination.tsx
+++ FE/src/utils/Pagination/Pagination.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import PaginationStyle from './css/Pagination.module.css';
+import PaginationStyle from './res/css/Pagination.module.css';
 
 export type PaginationType = {
     totalCnt : number;
@@ -11,85 +11,53 @@ export type PaginationType = {
 const Pagination = ({PaginationInfo} : {PaginationInfo : PaginationType}) => {
     
     const [CurIdx, setCurIdx] = useState(1);
-    const firstIdx = 1, lastIdx = Math.floor(PaginationInfo.totalCnt / PaginationInfo.perSize);
+    const FirstIdx = 1, LastIdx = Math.floor(PaginationInfo.totalCnt / PaginationInfo.perSize);
     let remain = CurIdx % PaginationInfo.range;
     const [PrevIdx, setPrevIdx] = useState(CurIdx - remain > 0 ? CurIdx - remain : 1);
-    const [NextIdx, setNextIdx] = useState(CurIdx - remain + PaginationInfo.range + 1 < lastIdx ? CurIdx - remain + PaginationInfo.range + 1 : lastIdx);
+    const [NextIdx, setNextIdx] = useState(CurIdx - remain + PaginationInfo.range + 1 < LastIdx ? CurIdx - remain + PaginationInfo.range + 1 : LastIdx);
     
     //처음 세팅
     const initList = [] as number[];
     for(let i= PrevIdx; i < NextIdx; i++) initList.push(i);
     const [CurList, setCurList] = useState<number[]>(initList);
     
-    function changeRange(idx: number) {
+    function paginationClick(idx: number) {
+        PaginationInfo.changeIdx(idx);
         remain = idx % PaginationInfo.range;
+        let tempPrev = 0, tempNext = 0;
         if(remain !== 0){
-            const tempPrev = idx - remain > 0 ? idx - remain : 1;
-            const tempNext = idx - remain + PaginationInfo.range + 1 < lastIdx ? idx - remain + PaginationInfo.range + 1 : lastIdx;
-
-            const tempList = [] as number[];
-            for(let i=tempPrev + 1; i < tempNext; i++) tempList.push(i);
-            if(tempPrev == 1) tempList.unshift(tempPrev);
-            if(tempNext == lastIdx) tempList.push(tempNext);
-            setCurList(tempList);
-            setPrevIdx(() => tempPrev);
-            setNextIdx(() => tempNext);
+            tempPrev = idx - remain > 0 ? idx - remain : 1;
+            tempNext = idx - remain + PaginationInfo.range + 1 < LastIdx ? idx - remain + PaginationInfo.range + 1 : LastIdx;
+        } else {
+            tempPrev = idx - PaginationInfo.range + 1;
+            tempNext = idx + 1 < LastIdx ? idx + 1 : LastIdx;
         }
+
+        const tempList = [] as number[];
+        for(let i=tempPrev + 1; i < tempNext; i++) tempList.push(i);
+        if(tempPrev == FirstIdx) tempList.unshift(tempPrev);
+        if(tempNext == LastIdx) tempList.push(tempNext);
+        setCurList(tempList);
+        setPrevIdx(() => tempPrev);
+        setNextIdx(() => tempNext);
+
         setCurIdx(() => idx);
         
     }
 
-    //첫번째로 가기
-    const goFirst = () => {
-        if(CurIdx != firstIdx){
-            PaginationInfo.changeIdx(firstIdx);
-            changeRange(firstIdx);
-        }
-        
-    }
-
-    //이전으로 가기
-    const goPrev = () => {
-        if(CurIdx != 1){
-            PaginationInfo.changeIdx(PrevIdx);
-            changeRange(PrevIdx);
-        }
-    }
-
-    //마지막으로 가기
-    const goLast = () => {
-        if(CurIdx != lastIdx){
-            PaginationInfo.changeIdx(lastIdx);
-            changeRange(lastIdx);
-        }
-    }
-
-    //다음으로 가기
-    const goNext = () => {
-        if(CurIdx != lastIdx){
-            PaginationInfo.changeIdx(NextIdx);
-            changeRange(NextIdx);
-        }
-    }
-
-    const clickIdx = (idx: number) => {
-        PaginationInfo.changeIdx(idx);
-        changeRange(idx);
-    }
-
     return (
-        <div className={`col-12 ${PaginationStyle.pagination_style_0_con}`}>
-            <button type="button" onClick={goFirst} className={`${PaginationStyle.arrow_btn} ${PaginationStyle.to_first}`}>첫번째로 가기</button>
-            <button type="button" onClick={goPrev} className={`${PaginationStyle.arrow_btn} ${PaginationStyle.to_prev}`}>이전으로 가기</button>
+        <div className={`col-12 tc ${PaginationStyle.pagination_style_0_con}`}>
+            <button type="button" onClick={() => paginationClick(FirstIdx)} className={`${PaginationStyle.arrow_btn} ${PaginationStyle.to_first}`}>첫번째로 가기</button>
+            <button type="button" onClick={() => paginationClick(PrevIdx)} className={`${PaginationStyle.arrow_btn} ${PaginationStyle.to_prev}`}>이전으로 가기</button>
             <ul className={`${PaginationStyle.pagination_style_0}`}>
                 {CurList.map((item, index) => (
                     item !== CurIdx && true ?
-                    (<li key={index} onClick={() => clickIdx(item)}>{item}</li>) :
+                    (<li key={index} onClick={() => paginationClick(item)}>{item}</li>) :
                     (<li key={index} className={`${PaginationStyle.active}`}>{item}</li>)
                 ))}
             </ul>
-            <button type="button" onClick={goNext} className={`${PaginationStyle.arrow_btn} ${PaginationStyle.to_next}`}>다음으로 가기</button>
-            <button type="button" onClick={goLast} className={`${PaginationStyle.arrow_btn} ${PaginationStyle.to_last}`}>마지막으로 가기</button>
+            <button type="button" onClick={() => paginationClick(NextIdx)} className={`${PaginationStyle.arrow_btn} ${PaginationStyle.to_next}`}>다음으로 가기</button>
+            <button type="button" onClick={() => paginationClick(LastIdx)} className={`${PaginationStyle.arrow_btn} ${PaginationStyle.to_last}`}>마지막으로 가기</button>
         </div>
     )
 }
