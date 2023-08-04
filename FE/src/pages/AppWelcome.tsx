b5a2b44b07b7f85563b5f4ec3c9918e1ab0d4908
Daen12 <sdy971213@gmail.com>
2023-08-04 09:14:58 +0900

    [DESIGN/front] 0804 DY welcome mp3 resized

--- FE/src/pages/AppWelcome.tsx
+++ FE/src/pages/AppWelcome.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import style from "@/res/css/module/AppWelcome.module.css"
-import video from '@/res/video/welcome_resized.mp4'
+import video from '@/res/video/welcome_resized2.mp4'
 // import "../res/video/Welcome.mp4"
 function Welcome() {
     const navigate = useNavigate();
