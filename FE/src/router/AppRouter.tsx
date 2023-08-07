6e592619af487f9dd6f9f12d2fa751f5e6194c0d
지민성 <jiminsung@naver.com>
2023-08-08 01:04:51 +0900

    [FEAT/front] 0808 MS mouse

--- FE/src/router/AppRouter.tsx
+++ FE/src/router/AppRouter.tsx
@@ -10,6 +10,9 @@ import FindPassword from "@/components/Login/FindPassword";
 import ChangePassword from "@/components/Login/ChangePassword";
 import AppHome from "@/pages/AppHome";
 import TimerApp from "@/components/home/Timer";
+import Mouse from '@/components/IDE/UserMouseCursor';
+
+
 const AppRouter = () => {
   return (
     <>
@@ -27,7 +30,7 @@ const AppRouter = () => {
           <Route path="/changepassword" element={<ChangePassword/>}/>
           <Route path="/home" element={<AppHome/>}/>
           <Route path="/timer" element={<TimerApp/>}/>
-
+          <Route path="/mouse/:id" element={<Mouse/>}/>
         </Routes>
       </BrowserRouter>
     </>
