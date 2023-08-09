0ac6aa65dd4a87986c3f1bf44267784edb424675
정민 <k3371548@naver.com>
2023-08-09 17:42:17 +0900

    [DESIGN/front] 0809 JM  home layout edit

--- FE/src/components/home/Timer.tsx
+++ FE/src/components/home/Timer.tsx
@@ -84,9 +84,9 @@ const handleResetClick = () => {
 
   return (
     <>
-    <div className={style.timer_container}>
-      <div className={style.progress_bar_container}>
-        <div className={style.custom_progress_bar}>
+    <div className={`col-12 ${style.timer_container}`}>
+      <div className={`col-6 ${style.progress_bar_container}`}>
+        <div className={`col-12 ${style.custom_progress_bar}`}>
         <CircularProgressbar 
           value={bar}
           
@@ -102,61 +102,61 @@ const handleResetClick = () => {
         />
         </div>
       </div>
-      <div className={style.controller}>
-      <div className={style.subtitle_timer} >타이머로 공부 시간을 관리해보세요</div>
-      <div className={style.timer_inputs}>
-        <input
-          type="number"
-          min="0"
-          value={timer.hours}
-          onChange={(e) =>
-            setTimer((prevTimer) => ({
-              ...prevTimer,
-              initialHours: parseInt(e.target.value, 10),
-              hours: parseInt(e.target.value, 10),
-            }))
-          }
-          disabled={timer.isRunning}
-        />
-        <span>hr</span>
-        <input
-          type="number"
-          min="0"
-          max="59"
-          value={timer.minutes}
-          onChange={(e) =>
-            setTimer((prevTimer) => ({
-              ...prevTimer,
-              initialMinutes: parseInt(e.target.value, 10),
-              minutes: parseInt(e.target.value, 10),
-            }))
-          }
-          disabled={timer.isRunning}
-        />
-        <span>min</span>
-        <input
-          type="number"
-          min="0"
-          max="59"
-          value={timer.seconds}
-          onChange={(e) =>
-            setTimer((prevTimer) => ({
-              ...prevTimer,
-              initialSeconds: parseInt(e.target.value, 10),
-              seconds: parseInt(e.target.value, 10),
-            }))
-          }
-          disabled={timer.isRunning}
-        />
-        <span>sec</span>
+      <div className={`col-6 ${style.controller}`}>
+        <div className={style.subtitle_timer} >타이머로 공부 시간을 관리해보세요</div>
+        <div className={style.timer_inputs}>
+          <input
+            type="number"
+            min="0"
+            value={timer.hours}
+            onChange={(e) =>
+              setTimer((prevTimer) => ({
+                ...prevTimer,
+                initialHours: parseInt(e.target.value, 10),
+                hours: parseInt(e.target.value, 10),
+              }))
+            }
+            disabled={timer.isRunning}
+          />
+          <span>hr</span>
+          <input
+            type="number"
+            min="0"
+            max="59"
+            value={timer.minutes}
+            onChange={(e) =>
+              setTimer((prevTimer) => ({
+                ...prevTimer,
+                initialMinutes: parseInt(e.target.value, 10),
+                minutes: parseInt(e.target.value, 10),
+              }))
+            }
+            disabled={timer.isRunning}
+          />
+          <span>min</span>
+          <input
+            type="number"
+            min="0"
+            max="59"
+            value={timer.seconds}
+            onChange={(e) =>
+              setTimer((prevTimer) => ({
+                ...prevTimer,
+                initialSeconds: parseInt(e.target.value, 10),
+                seconds: parseInt(e.target.value, 10),
+              }))
+            }
+            disabled={timer.isRunning}
+          />
+          <span>sec</span>
+        </div>
+        <div className={style.timer_buttons}>
+          <button onClick={handleStartStopClick}>{timer.isRunning ? "Stop" : "Start"}</button>
+          <button onClick={handleResetClick} disabled={timer.isRunning}>
+            Reset
+          </button>
+        </div>
       </div>
-      <div className={style.timer_buttons}>
-        <button onClick={handleStartStopClick}>{timer.isRunning ? "Stop" : "Start"}</button>
-        <button onClick={handleResetClick} disabled={timer.isRunning}>
-          Reset
-        </button>
-      </div>
-    </div>
     </div>
     </>
   );
--- FE/src/pages/AppHome.tsx
+++ FE/src/pages/AppHome.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
-import Background from '@/components/home/Background.tsx';
+import homeBgImg from '@/res/img/home_background.jpg';
 import Lnb from '@/components/inc/Lnb';
+import Comedy from '@/components/home/Comedy';
+import Timer from '@/components/home/Timer';
+import CalendarApp from '@/components/home/Calendar';
+import Schedule from '@/components/home/Schedule';
 const AppHome = () => {
     return (
-        <>
-        
-        <Background></Background>
-        {/* <Lnb></Lnb> */}
-
-        </>
+        <div className="col-12 sub_wrap">
+            <div className="col-12 sub_con" style={{backgroundImage: `url(${homeBgImg})`}}>
+                <Lnb/>
+                <div className="col-12">
+                    <div className="col-12 col-md-7 pl-md-15 pr-md-15">
+                        <div className="col-12 mb20"><Comedy></Comedy></div>
+                        <div className="col-12"><Timer></Timer></div>
+                    </div>
+                    <div className="col-12 col-md-5 pl-md-15 pr-md-15">
+                        <CalendarApp></CalendarApp>
+                        <Schedule></Schedule>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
 
