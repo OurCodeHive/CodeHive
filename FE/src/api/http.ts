fd3db8b8485503cf8f309c9554a582504053410c
KimMinSik <rlaalstlr0108@gmail.com>
2023-08-10 21:15:42 +0900

    [FEAT/front] KMS 0810 Update http.ts

--- FE/src/api/http.ts
+++ FE/src/api/http.ts
@@ -5,105 +5,41 @@
 */
 import {Cookies} from 'react-cookie';
 import axios, { AxiosInstance, AxiosResponse, InternalAxiosRequestConfig} from "axios";
+import moment from 'moment';
 
 interface ICookies {
     get(e:string):string,
 }
-interface IResponse extends AxiosResponse{
-    data : {
-        accessToken : string,
-    }
-}
 
-const cookies:ICookies = new Cookies();
-
-
-// interface APIResponse<T> {
-//     statusCode: number // 상태코드 (보인 서버상태코드)
-//     errorCode: number // 에러코드 (본인 서버에러코드)
-//     message: string // 메시지
-//     result: T // 데이터 내용
-//     timestamp: Date // 시간
-//   }
-
-//인증이 필요한 axios instance
-const authHttp =  axios.create({
-    baseURL: import.meta.env.VITE_APP_SERVER as string,
-    timeout: 10000,
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    withCredentials : true,
-});
-
-let accessToken : string|null = localStorage.getItem("accessToken");
-
-authHttp.interceptors.request.use(
-    (config : InternalAxiosRequestConfig):InternalAxiosRequestConfig => {
-        if (config.headers){
-            config.headers.Authorization = 'Bearer ' + localStorage.getItem("accessToken");
-            //console.log(config);
-        }
-      return config;
-    }
-)
 interface IResponse extends AxiosResponse {
     response?: {
        data?: {
-        accessToken?:string
+        accessToken?:string,
        };
        status: number;
        headers: string;
     };
  }
-// authHttp.interceptors.response.use(
-    
-//     async (response : IResponse): Promise<any> => {
 
-    
-//       const { config, status } = response; //response의 config 파일
-//       const originalRequest = config;
-//       console.log(response);
-//       if(response.data.accessToken){//반환값에 access token이 있으면
-//         const accessToken = response.data.accessToken;
-//         localStorage.setItem("accessToken", accessToken);
-//         originalRequest.headers.Authorization = `Bearer ${accessToken}`
-//         console.log("access renewed!");
-//         return await axios(originalRequest).then((res)=>{
-//             console.log(res);
-//         }).catch((err)=>{
-//             console.log("errr");
-//             console.log(err);
-//         })
-//       }
-//     //   if (status === 403) {//에러 = 엑세스 토큰 만료. 확인 후 validate
-//     //     const refreshToken = cookies.get("refreshToken");
-//     //     const reIssueData = {
-//     //         accessToken : accessToken,
-//     //         refreshToken : refreshToken
-//     //     }
-//     //     return await authHttp.post(`reissue`, reIssueData)
-//     //       .then((res:IResponse) => {
-//     //         if(res.status == 200){
-//     //             accessToken = res.data.accessToken;
-//     //             localStorage.setItem("accessToken", accessToken);
-//     //             originalRequest.headers.Authorization = `Bearer${accessToken}`;
-//     //             return axios(originalRequest);
-//     //         }
-//     //       }).catch((err) => {
-//     //         console.log(err)
-//     //       })
-//     //   }
-//       return response;
-//     },
-//     // 에러
-//     (error) => {
-//       console.log(error, '^^***')
-//       throw error
-//     }
-//   )
+const cookies:ICookies = new Cookies();
 
-//인증이 불필요한 axios instance
+async function reissue(accessToken: string | null) {
+    await axios.create({
+        baseURL: import.meta.env.VITE_APP_SERVER as string,
+        timeout: 10000,
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + accessToken,
+        },
+        withCredentials : true,
+    }).post(`/reissue`, {
+        accessToken : accessToken,
+    }).then((res) => {
+        localStorage.setItem('accessToken', res.data.accessToken);
+        localStorage.setItem('expireAt', moment().add(3, "minute").format("yyyy-MM-DD HH:mm:ss"))
+    }).catch(() => {})
+}
+
 const nonAuthHttp : AxiosInstance = axios.create({
     baseURL : import.meta.env.VITE_APP_SERVER as string,
     timeout: 10000,
@@ -113,22 +49,81 @@ const nonAuthHttp : AxiosInstance = axios.create({
     withCredentials : true,
 })
 
-//인증이 불필요한 axios instance
+const authHttp = axios.create({
+    baseURL: import.meta.env.VITE_APP_SERVER as string,
+    timeout: 10000,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    withCredentials : true,
+});
+
+authHttp.interceptors.request.use(
+    (config : InternalAxiosRequestConfig):InternalAxiosRequestConfig => {
+        const expireAt = localStorage.getItem('expireAt');
+        const accessToken: string | null = localStorage.getItem('accessToken');
+        
+        if (moment().isAfter(expireAt)) {
+            reissue(accessToken);
+        }
+
+        if (config.headers && accessToken){
+            config.headers.Authorization = `Bearer ${accessToken}`;
+        }
+
+      return config;
+    }
+)
+
+// authHttp.interceptors.response.use(
+//     async (response : IResponse): Promise<any> => {
+//       const { config, status } = response; //response의 config 파일
+//       const originalRequest = config;
+
+//       if (response.data.accessToken && response.data.grantType === "Bearer") {
+//         const accessToken = response.data.accessToken;
+//         localStorage.setItem("accessToken", accessToken);
+//         originalRequest.headers.Authorization = `Bearer ${accessToken}`
+//         console.log("access renewed!");
+//         return await axios(originalRequest).then((res)=>{
+//             console.log(res);
+//         }).catch((err) => {
+//             console.log("errr");
+//             console.log(err);
+//         })
+//       }
+//       return response;
+//     },
+//     // 에러
+//     (error) => {
+//       console.log(error, '^^***')
+//       throw error
+//     }
+//   )
+
 const formHttp : AxiosInstance = axios.create({
     baseURL : import.meta.env.VITE_APP_SERVER as string,
     timeout: 10000,
     headers: {
         'Content-Type': 'multipart/form-data',
     },
+    withCredentials : true,
 })
 
 formHttp.interceptors.request.use(
     (config : InternalAxiosRequestConfig):InternalAxiosRequestConfig => {
-        if(config.headers && accessToken){
-            config.headers.Authorization = `Bearer ${accessToken}`;
-            console.log(config);
-        }
-      return config;
+      const expireAt = localStorage.getItem('expireAt');
+      const accessToken: string | null = localStorage.getItem('accessToken');
+      
+      if (moment().isAfter(expireAt)) {
+          reissue(accessToken);
+      }
+
+      if (config.headers && accessToken){
+          config.headers.Authorization = `Bearer ${accessToken}`;
+      }
+
+    return config;
     }
 )
 
