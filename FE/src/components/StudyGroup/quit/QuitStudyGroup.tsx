f8cd5083bdb6994fa39294f005651c8d79b5a155
gleehave <gleehave@gmail.com>
2023-08-11 09:36:31 +0900

    [FEAT/front] GH 230811 Study Group Quit Flow

--- FE/src/components/StudyGroup/quit/QuitStudyGroup.tsx
+++ FE/src/components/StudyGroup/quit/QuitStudyGroup.tsx
@@ -6,7 +6,7 @@ function QuitStudyGroupButton(props:any){
 
     const userInfo = useRecoilValue(userState);
 
-    const handleStudyQuit = () => {
+    const handleStudyQuitYes = () => {
         fetch("http://localhost:8080/api/study/leave", {
             method: "POST",
             headers: {
@@ -19,13 +19,36 @@ function QuitStudyGroupButton(props:any){
           })
             .then(response => response.json())
             .then(data => {
+              
+              if (data.isLeave){
+                // 스터디 탈퇴 시, 자료를 공유받을 수 없습니다. 그럼에도 탈퇴하시겠습니까? (예 / 아니오)
+                console.log("스터디 탈퇴 시, 자료를 공유받을 수 없습니다. 그럼에도 탈퇴하시겠습니까? (예 / 아니오)");
+              }
+
+              if (!data.isLeave){
+                console.log("스터디 ") 
+              }
+
+
+
             })
             .catch(error => {
               console.error("Error:", error);
             });
     };
 
-    return (<button onClick={handleStudyQuit}></button>);
+    const handleStudyQuitNo = () => {
+      // 아무것도 없거나.. 
+      // 다시 스터디 상세 화면으로 가게끔 뭘 해줘야할거같은데..?
+    }
+
+    return (
+      <>
+        스터디 탈퇴 시, 자료를 공유받을 수 없습니다. 그럼에도 탈퇴하시겠습니까?
+        <button type="button" onClick={handleStudyQuitYes}>예</button>
+        <button type="button" onClick={handleStudyQuitNo}>아니오</button>
+      </>
+    );
 }
 
 export default QuitStudyGroupButton;
