86a8b0965b86285a3752e439f15288ccf2cbb77f
Daen12 <111489407+Daen12@users.noreply.github.com>
2023-08-13 22:37:25 +0900

    [FEAT/front] 0813 DY main header added

--- FE/src/pages/AppHome.tsx
+++ FE/src/pages/AppHome.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import homeBgImg from '@/res/img/home_background.jpg';
 import HomeStyle from '@/res/css/page/AppHome.module.css';
 import Lnb from '@/components/inc/Lnb';
@@ -6,20 +6,46 @@ import Comedy from '@/components/home/Comedy';
 import Timer from '@/components/home/Timer';
 import CalendarApp from '@/components/home/Calendar';
 import Schedule from '@/components/home/Schedule';
+import Intro from '@/components/home/Intro';
+import StudyViewMenu from '@/components/StudyGroup/menu/Menu';
+import { useRecoilValue } from 'recoil';
+import { useNavigate } from 'react-router-dom';
+import { userState } from '@/atom/UserAtom';
+
 const AppHome = () => {
+    let [day, setDay] = useState<string>("");
+    let loginUser = useRecoilValue(userState);
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+        let hours = new Date().getHours();
+        if (hours < 12 && hours >= 0) {
+           setDay("아침이에요");
+        } else if (hours >= 12 && hours <= 18) {
+            setDay("오후에요");
+        } else if (19 <= hours && hours < 24) {
+            setDay("저녁이에요");
+        }
+    },[])
     return (
         <div className="col-12 sub_wrap">
             <div className="col-12 sub_con" style={{backgroundImage: `url(${homeBgImg})`}}>
                 <div className="col-12 sub_contents" style={{backgroundColor: "#ffffffa0"}}>    
                     <Lnb/>
                     <div className="col-12">
-                        <div className="col-12 col-md-6 pl-md-15 pr-md-15">
-                            <div className="col-12 mb20"><Comedy></Comedy></div>
-                            <div className="col-12"><Timer></Timer></div>
+                        <div className="col-12">
+                            <StudyViewMenu/>
+                        </div>
+                        <div className={`col-12 mb30 ml50 ${HomeStyle.greeting}`}>
+                            좋은 {day}, {loginUser.nickname}님
                         </div>
                         <div className="col-12 col-md-6 pl-md-15 pr-md-15">
-                            <div className="col-12 mb20"><CalendarApp></CalendarApp></div>
-                            <div className="col-12"><Schedule></Schedule></div>
+                            <div className="col-12 mb50 ml30"><Comedy></Comedy></div>
+                            <div className="col-12 ml30"><Timer></Timer></div>
+                        </div>
+                        <div className="col-12 col-md-6 pl-md-15 pr-md-15">
+                            <div className="col-12 mb20 ml30"><CalendarApp></CalendarApp></div>
+                            <div className="col-12 ml30"><Schedule></Schedule></div>
                         </div>
                     </div>
                 </div>
