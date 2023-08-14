b7f8f3c3392b9df6c7c5927cfd44980bf9927159
Daen12 <sdy971213@gmail.com>
2023-08-14 12:50:08 +0900

    [FEAT/front] 0814 DY timer global sync

--- FE/src/pages/AppStudy.tsx
+++ FE/src/pages/AppStudy.tsx
@@ -1,8 +1,50 @@
+import { useTimerState } from "@/atom/TimerAtom";
 import Lnb from "@/components/inc/Lnb";
 import StudyView from "@/components/StudyGroup/view";
 import StudyViewBgImg from '@/res/img/codehive_study_view_bg_img.png';
+import { useEffect } from "react";
 
 function AppStudy() {
+  const { timer, setTimer, startTimer, stopTimer, resetTimer } = useTimerState();
+    useEffect(() => {
+        let interval: NodeJS.Timeout;
+    
+        if (timer.isRunning) {
+          interval = setInterval(() => {
+            if (timer.seconds > 0) {
+                setTimer((prevTimer) => ({
+                ...prevTimer,
+                seconds: prevTimer.seconds - 1,
+              }));
+            } else {
+              if (timer.minutes > 0) {
+                setTimer((prevTimer) => ({
+                  ...prevTimer,
+                  minutes: prevTimer.minutes - 1,
+                  seconds: 59,
+                }));
+              } else {
+                if (timer.hours > 0) {
+                    setTimer((prevTimer) => ({
+                    ...prevTimer,
+                    hours: prevTimer.hours - 1,
+                    minutes: 59,
+                    seconds: 59,
+                  }));
+                } else {
+                    setTimer((prevTimer) => ({
+                    ...prevTimer,
+                    isRunning: false,
+                  }));
+                  alert("Timer has ended!");
+                }
+              }
+            }
+          }, 1000);
+        }
+    
+        return () => clearInterval(interval);
+      }, [timer, setTimer]);
   return (
     <div className="col-12 sub_wrap">
       <div className="col-12 sub_con" style={{backgroundImage: `url(${StudyViewBgImg})`}}>
