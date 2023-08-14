f6d92248a4a95eefe557db2c96f71b73ff53e2e2
Daen12 <sdy971213@gmail.com>
2023-08-14 11:03:11 +0900

    [FEAT/front] 0814 DY ongoing study exception

--- FE/src/components/home/Schedule.tsx
+++ FE/src/components/home/Schedule.tsx
@@ -19,11 +19,10 @@ const Schedule = () => {
 
     useEffect(()=>{
         const today = new Date().toISOString().slice(0,10)
-        // const date = "2023-08-09";
         authHttp.get<ScheduleData>(`/today/study?today=${today}`).then((res)=>{
             const {today} = res.data;
             if(today){
-                setData(today);//[{},{},{}]
+                setData(today);
             }
         }).catch(console.log);
     },[])
@@ -48,16 +47,18 @@ const ScheduleListItem = ({data} : {data: ISchedule}) => {
           </div>
           <div className={`col-12 ${style.duration}`}>{`${getDuration(data.startTime, data.endTime)[0]}hr ${getDuration(data.startTime, data.endTime)[1]}min`}</div>
         </div>
-        <div className={`col-12 ${style.timer_text}`}>{getTimeLeft(data.startTime)}</div>
+        <div className={`col-12 ${style.timer_text}`}>{getTimeLeft(data.startTime, data.endTime)}</div>
       </div>
     );
   };
+
   function getUTCTime(){
-    const curr = new Date();
+    // const curr = new Date();
     const offset = 1000 * 60 * 60 * 9
     const seoul = new Date((new Date()).getTime() + offset)
     return seoul.toISOString();
   }
+
   function getDuration(startTime:string, endTime:string):number[]{
     const startAt = moment(startTime);
     const endAt = moment(endTime);
@@ -65,19 +66,33 @@ const ScheduleListItem = ({data} : {data: ISchedule}) => {
     const mins = moment.duration(endAt.diff(startAt)).minutes();
     return[hrs,mins];
   }
-  function getTimeLeft(startTime:string | undefined):string{
+
+  function getTimeLeft(startTime:string | undefined, endTime:string | undefined):string{
+    //현재 한국 시간
     let now = getUTCTime();
+    //시작시간, 끝 시간을 한국 시간으로 계산
     let startAt = moment(moment(startTime).valueOf() + 9 * 60 * 60 * 1000);
+    let endAt = moment(moment(endTime).valueOf() + 9 * 60 * 60 * 1000);
     // console.log("now = "+now);
     // console.log("startAt = "+startAt.toISOString());
+
+    //현재 시간과 시작시간의 차이를 계산
     let hours = moment.duration(startAt.diff(moment(now))).hours();
     let mins = moment.duration(startAt.diff(moment(now))).minutes();
     let seconds = moment.duration(startAt.diff(moment(now))).seconds();
-    // let duration = moment(moment.utc(moment(startAt)).diff(moment(now))).days();
-    if(hours<=0 && mins<=0 && seconds<=0){
-        return "지난 스터디입니다";
+
+    //현재 시간과 끝 시간의 차이를 계산
+    let endHours = moment.duration(endAt.diff(moment(now))).hours();
+    let endMins = moment.duration(endAt.diff(moment(now))).minutes();
+    let endSeconds = moment.duration(endAt.diff(moment(now))).seconds();
+
+    //계산한 데이터를 바탕으로 예외 처리
+    if((hours<=0 && mins<=0 && seconds<=0) && (endHours>0 || endMins>0 || endSeconds>0)){
+        return "진행중인 스터디입니다";
+    } else if (hours<=0 && mins<=0 && seconds<=0) {
+      return "지난 스터디입니다";
     } else {
-        return `남은 시간:${hours}시간 ${mins}분`;
+        return `남은 시간 : ${hours}시간 ${mins}분`;
     }
 }
 
